40fa58fa09a7453e66dd95ce6149e91fb9948496
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-08-27 20:40:24 -0300

    ver depois

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,7 +4,17 @@ import styles from './home_styles';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Início</Text>
+      <View style={styles.containerDiario}>
+        <Text style={styles.titleDiario}>Diario</Text>
+      </View>
+
+      <View style={styles.containerEmocao}>
+        <Text style={styles.titleEmocao}>Emoção</Text>
+      </View>
+
+      <View style={styles.containerAtividades}>
+       <Text style={styles.titleAtividades}>Atividades</Text>
+      </View>
     </View>
   );
 }
--- src/screens/Home/home_styles.js
+++ src/screens/Home/home_styles.js
@@ -3,13 +3,40 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
     },
-    title: {
+    containerDiario: {
+      backgroundColor: '#000',
+      padding: 20, 
+      margin: 60,
+      borderRadius: 20,
+      width: '80%'
+    },
+    containerEmocao: {
+      backgroundColor: '#000',
+      padding: 20, 
+      width: '80%',
+      borderRadius: 20,
+      marginBottom: 20,
+    },
+    containerAtividades: {
+      backgroundColor: '#000',
+      padding: 20, 
+      width: '80%',
+      borderRadius: 20,
+    },
+    titleDiario: {
+      fontSize: 22,
+      fontWeight: 'bold',
+    },
+    titleEmocao: {
+      fontSize: 22,
+      fontWeight: 'bold',
+    },
+    titleAtividades: {
       fontSize: 22,
       fontWeight: 'bold',
     },
   });
+
   export default styles;
