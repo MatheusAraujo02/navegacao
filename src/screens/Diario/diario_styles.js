07157ea6a8d8b6799e56ce33cf20b5120c6d3453
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-09-06 20:41:28 -0300

    Update calendario

--- src/screens/Diario/diario_styles.js
+++ src/screens/Diario/diario_styles.js
@@ -3,18 +3,17 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        //   alignItems: 'center',
-        padding: 20,
+        // padding: 20,
         backgroundColor: '#f0f0f0',
+        // justifyContent: 'center'
+        alignItems: 'center'
     
     },
-    calendario: {
-        borderWidth: 1,
-        borderColor: 'grey',
-        borderRadius: 15,
-        position: 'relative'
+    calendario: {   
+        
     },
     miniDiario: {
+        width: '90%',
         marginTop: 20,
         padding: 20,
         backgroundColor: '#fff',
@@ -29,7 +28,7 @@ const styles = StyleSheet.create({
         fontSize: 18
         },
     textoPerguntas: {
-        justifyContent: 'space-around',
+        // justifyContent: 'space-around',
         marginTop: 10,
         margin: 10,
         marginLeft: 0,
