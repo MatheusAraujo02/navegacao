ac9009d6c60d6982fd3125e5d3112022961aa348
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-09-06 22:43:36 -0300

    Calendario
    Tentando fazer o dia selecionado aparecer

--- src/screens/Diario/Diario.js
+++ src/screens/Diario/Diario.js
@@ -1,31 +1,37 @@
 import { useState} from 'react';
 import { Text, View, Pressable } from 'react-native';
-import {Calendar, LocaleConfig, DateData } from 'react-native-calendars'
-import { DayState } from 'react-native-calendars/src/types';
+import {Calendar, DateString} from 'react-native-calendars'
 
-// import { ptBR } from '../../components/localeCalendarConfig';
 import styles from './diario_styles';
 
-// LocaleConfig.locales['pt-Br'] = ptBR
-// LocaleConfig.defaultLocale = 'pt-br'
 
-const Data = "Quinta"
-const Dia = "04"
-const Mes = "Setembro"
 
 export default function Diario() {
-  // const [day, setDay] = useState<DateData>() 
+  
+  const [day, setDay] = useState()
   
   return (
     <View style={styles.container}>
       <Calendar 
       style={styles.calendario}
-      onDayPress={console.log}
+      theme={{
+        monthTextColor: '#000',
+        todayTextColor: 'darkgreen',
+        selectedDayBackgroundColor :'darkgreen',
+        selectedDayTextColor: '#fff',
+      }}
+      onDayPress={setDay}
+      markedDates={
+        day && {
+          [day.DateString]: {selected: true} 
+      }}
+
+      
 
       
       />
       <View style={styles.miniDiario}>
-        <Text style={styles.textoData}> {Data}, {Dia} de {Mes} </Text> 
+        <Text style={styles.textoData}>  {day} </Text> 
         <Text style={[styles.textoPerguntas, {marginTop: 40}]}> O que te fez bem? </Text> 
         <Text style={styles.textoPerguntas}> O que te fez mal? </Text> 
         <Text style={styles.textoPerguntas}> Quais pensamentos? </Text> 
